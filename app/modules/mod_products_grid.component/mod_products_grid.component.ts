7196246e3c812100f37da5803889df7df14cf0d3
hugo <mbiz.hugo@gmail.com>
2016-10-19 10:53:32 +0700

    hide product

--- app/modules/mod_products_grid.component/mod_products_grid.component.ts
+++ app/modules/mod_products_grid.component/mod_products_grid.component.ts
@@ -9,20 +9,20 @@ import { Product } from '../../model/product/product';
 export class ModProductsGridComponent implements OnInit {
     list_product: Product[];
     constructor() {
-        this.list_product = [
-            {product_id: '1', product_name: 'white T-Shirt', product_img: 'images/p1.jpg', price: 300,
-             brief: 'Wattle seed bunya nuts spring onion okra garlic bitterleaf zucchini'},
-            {product_id: '2', product_name: 'yellow T-Shirt', product_img: 'images/p2.jpg', price: 500,
-             brief: 'A classic fifties shape is updated with the Floral Fit-and-Flare Dress from Wallis'},
-            {product_id: '3', product_name: 'black skirt', product_img: 'images/p3.jpg', price: 250,
-             brief: 'The Keep On Swinging Wrap Dress from Lioness is crafted from a silky-smooth and lightweight fabric'},
-            {product_id: '4', product_name: 'red dress', product_img: 'images/p4.jpg', price: 450,
-             brief: 'Add a retro vibe to your wardrobe with the Meeka Ruffle Dress from Atmos&Here'},
-            {product_id: '5', product_name: 'gray dress', product_img: 'images/p5.jpg', price: 367,
-             brief: 'Wrap dresses are here to stay, so look to the Atmos&Here Antonia Wrap Dress'},
-            {product_id: '6', product_name: 'blue Shirt', product_img: 'images/p6.jpg', price: 352, 
-             brief: 'The Rosie Wrap Dress from Atmos&Here is crafted from a lightweight fabric with a romantic floral print'},
-        ]
+        // this.list_product = [
+        //     {product_id: '1', product_name: 'white T-Shirt', product_img: 'images/p1.jpg', price: 300,
+        //      brief: 'Wattle seed bunya nuts spring onion okra garlic bitterleaf zucchini'},
+        //     {product_id: '2', product_name: 'yellow T-Shirt', product_img: 'images/p2.jpg', price: 500,
+        //      brief: 'A classic fifties shape is updated with the Floral Fit-and-Flare Dress from Wallis'},
+        //     {product_id: '3', product_name: 'black skirt', product_img: 'images/p3.jpg', price: 250,
+        //      brief: 'The Keep On Swinging Wrap Dress from Lioness is crafted from a silky-smooth and lightweight fabric'},
+        //     {product_id: '4', product_name: 'red dress', product_img: 'images/p4.jpg', price: 450,
+        //      brief: 'Add a retro vibe to your wardrobe with the Meeka Ruffle Dress from Atmos&Here'},
+        //     {product_id: '5', product_name: 'gray dress', product_img: 'images/p5.jpg', price: 367,
+        //      brief: 'Wrap dresses are here to stay, so look to the Atmos&Here Antonia Wrap Dress'},
+        //     {product_id: '6', product_name: 'blue Shirt', product_img: 'images/p6.jpg', price: 352, 
+        //      brief: 'The Rosie Wrap Dress from Atmos&Here is crafted from a lightweight fabric with a romantic floral print'},
+        // ]
      }
 
     ngOnInit() { }
--- app/modules/mod_related_products.component/mod_related_products.component.ts
+++ app/modules/mod_related_products.component/mod_related_products.component.ts
@@ -9,14 +9,14 @@ import { Product } from '../../model/product/product';
 export class ModRelatedProductsComponent implements OnInit {
     list_product: Product[];
     constructor() {
-        this.list_product = [
-            {product_id: '1', product_name: 'white T-Shirt', product_img: 'images/p1.jpg', price: 300,
-             brief: 'Wattle seed bunya nuts spring onion okra garlic bitterleaf zucchini'},
-            {product_id: '2', product_name: 'yellow T-Shirt', product_img: 'images/p2.jpg', price: 500,
-             brief: 'A classic fifties shape is updated with the Floral Fit-and-Flare Dress from Wallis'},
-            {product_id: '3', product_name: 'black skirt', product_img: 'images/p3.jpg', price: 250,
-             brief: 'The Keep On Swinging Wrap Dress from Lioness is crafted from a silky-smooth and lightweight fabric'},
-        ]
+        // this.list_product = [
+        //     {product_id: '1', product_name: 'white T-Shirt', product_img: 'images/p1.jpg', price: 300,
+        //      brief: 'Wattle seed bunya nuts spring onion okra garlic bitterleaf zucchini'},
+        //     {product_id: '2', product_name: 'yellow T-Shirt', product_img: 'images/p2.jpg', price: 500,
+        //      brief: 'A classic fifties shape is updated with the Floral Fit-and-Flare Dress from Wallis'},
+        //     {product_id: '3', product_name: 'black skirt', product_img: 'images/p3.jpg', price: 250,
+        //      brief: 'The Keep On Swinging Wrap Dress from Lioness is crafted from a silky-smooth and lightweight fabric'},
+        // ]
      }
     ngOnInit() { }
 }
